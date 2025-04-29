5defce83d0978ff53fe6890c5923f0df87d744f0
Dev Jadiya <devjadiya15@gmail.com>
2025-04-29 07:40:32 +0530

    Updated

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -3,7 +3,7 @@ import { companyLogos } from "../constants";
 const CompanyLogos = ({ className }) => {
   return (
     <div className={className}>
-      <h5 className="tagline mb-6 text-center text-n-1/50">learn at Gurukul</h5>
+      <h5 className="tagline mb-6 text-center text-n-1/50">learn at CollabSmart</h5>
       <ul className="flex">
         {companyLogos.map((logo, index) => (
           <li
